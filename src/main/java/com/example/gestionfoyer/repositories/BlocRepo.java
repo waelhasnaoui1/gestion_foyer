798c84a5b1a117fd3b46de5ba3a574f857dbf450
waelhasnaoui1 <waelhasnaoui006@gmail.com>
2024-07-08 10:05:28 +0100

    .

--- src/main/java/com/example/gestionfoyer/repositories/BlocRepo.java
+++ src/main/java/com/example/gestionfoyer/repositories/BlocRepo.java
@@ -2,8 +2,16 @@ package com.example.gestionfoyer.repositories;
 
 import com.example.gestionfoyer.entities.Bloc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BlocRepo extends JpaRepository<Bloc,Long> {
+
+    @Query("SELECT b FROM Bloc b WHERE b.foyer.university.idUniversite =: idUniversite")
+    List<Bloc> findByFoyerUniversityIdUniversite(@Param("idUniversite") long idUniversite);
+
 }
--- src/main/java/com/example/gestionfoyer/repositories/ChambreRepo.java
+++ src/main/java/com/example/gestionfoyer/repositories/ChambreRepo.java
@@ -1,9 +1,24 @@
 package com.example.gestionfoyer.repositories;
 
 import com.example.gestionfoyer.entities.Chambre;
+import com.example.gestionfoyer.entities.TypeChambre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChambreRepo extends JpaRepository<Chambre,Long> {
+
+    List<Chambre> findByBlocFoyerUniversityNomUniversite(String nomUniversite);
+
+    List<Chambre> findByBlocIdBlocAndTypeC(long idBloc, TypeChambre typeC);
+
+    @Query("SELECT c from Chambre c WHERE c.bloc.idBloc =:idBloc and c.typeC =:typeChambre ")
+    List<Chambre> findByBlocAndTypeCJPQL(long idBloc,TypeChambre typeChambre);
+
+    @Query("SELECT c from Chambre c join Reservation r on(r member of c.reservationSet) where (c.bloc.idBloc:= idBloc and r.idReservation not like cast(year(current_date) as string)) order by c.idChambre limit 1 ")
+    Chambre getForReservation(long idBloc);
+
 }
